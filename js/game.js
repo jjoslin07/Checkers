e572a72781a54e5b50f3d6b9c21bf29e2a554a97
Joseph Joslin <jjoslin07@gmail.com>
2021-07-02 23:38:50 -0700

    Changes the board states data on the back end

--- js/game.js
+++ js/game.js
@@ -335,3 +335,32 @@ function makeMove(number) {
         changeData(indexOfPiece, indexOfPiece + numnber);
     }
 }
+
+/**
+ * Changes the board states data on the back end
+ */
+
+function changeData(indexOfBoardPiece, modifiedIndex, removePiece) {
+    board[indexOfBoardPiece] = null;
+    board[modifiedIndex] = parseInt(selectedPiece.pieceId);
+    if (turn && selectedPiece.pieceId < 12 && modifiedIndex >= 57) {
+        document.getElementById(selectedPiece.pieceId).classList.add("king");
+    }
+    if (turn === false && selectedPiece.pieceId >= 12 && modifiedIndex <= 7) {
+        document.getElementById(selectedPiece.pieceId).classList.add("king");
+    }
+    if (removePiece) {
+        board[removePiece] = null;
+        if (turn && selectedPiece.pieceId < 12) {
+            cells[removePiece].innerHTML = "";
+            blackScore--
+        }
+        if (turn === false && selectedPiece.pieceId >= 12) {
+            cells[removePiece].innerHTML = "";
+            redScore--
+        }
+    }
+    resetSelectedPieceProperties();
+    removeCellonclick();
+    removeEventListeners();
+}
