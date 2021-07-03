fd991a6574e9c57428675250962dd0fe6cdcda2f
Joseph Joslin <jjoslin07@gmail.com>
2021-07-03 00:19:30 -0700

    Change file structure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Game State Data
  */
 
-const board = [
+ const board = [
     null, 0, null, 1, null, 2, null, 3, 
     4, null, 5, null, 6, null, 7, null, 
     null, 8, null, 9, null, 10, null, 11, 
